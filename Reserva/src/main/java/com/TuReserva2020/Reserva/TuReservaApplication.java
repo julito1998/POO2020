315d031b4ab26ed4c5195107b276a5e8cf809f0f
julito <zabalajulian2010@hotmail.com>
2020-11-03 09:54:11 -0300

    primer_commit

---
+++ Reserva/src/main/java/com/TuReserva2020/Reserva/TuReservaApplication.java
@@ -0,0 +1,13 @@
+package com.TuReserva2020.Reserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuReservaApplication.class, args);
+	}
+
+}
---
+++ Reserva/src/test/java/com/TuReserva2020/Reserva/TuReservaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TuReserva2020.Reserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuReservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
