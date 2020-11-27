85b6c6311eefd9aebaa09b201fc079a10b150b94
facu <facufigliuolo@gmail.com>
2020-11-27 17:01:59 -0300

    acomode cosas en DTO y saqué empty html

--- Reserva/src/main/java/com/TuReserva2020/Reserva/Controller/BookingController.java
+++ Reserva/src/main/java/com/TuReserva2020/Reserva/Controller/BookingController.java
@@ -34,7 +34,7 @@ public class BookingController {
 
     @GetMapping("/availability")
     private String roomsAvailabily(Model model){
-        model.addAttribute("roomAvailability", new RoomAvailabilityDTO());
+        model.addAttribute("roomsAvailability", new RoomAvailabilityDTO());
         model.addAttribute("rooms",new RoomDTO());
         return "bookings/availability";
     }
@@ -44,8 +44,8 @@ public class BookingController {
         List<Room> rooms = new ArrayList<>();
         try{
             rooms= serviceRoom.getRoomsAvailable(
-                    roomsAvailabilityDTO.getCheckInDateConverted("yyyy-MM-dd"),
-                    roomsAvailabilityDTO.getCheckOutDateConverted("yyyy-MM-dd"),
+                    roomsAvailabilityDTO.getCheckInDateConverted(),
+                    roomsAvailabilityDTO.getCheckOutDateConverted(),
                     roomsAvailabilityDTO.getOccupancy());
         }catch (Exception e){}
 
