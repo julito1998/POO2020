1e32719c0fa7b26074b30b6abe0e4f28aea8bbf5
julito1998 <zabalajulian2010@hotmail.com>
2021-03-27 01:47:24 -0300

    appController

--- Reserva/src/main/java/com/TuReserva2020/Reserva/Controller/AppController.java
+++ Reserva/src/main/java/com/TuReserva2020/Reserva/Controller/AppController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 @RequestMapping("/")
 public class AppController {
 
